fa04685b8e215e36c04bc8758eba8a7c056d2cd6
ilm75862 <2540395301qq.com>
2025-05-10 10:44:41 +0800

    add janusAgent and deepseek-vl2-tiny

--- avp_env/agents/LLM_agent.py
+++ avp_env/agents/LLM_agent.py
@@ -2,19 +2,126 @@ import openai
 import base64
 import io
 import os
-
 import torch
 from PIL import Image
 from transformers import AutoModelForCausalLM, Qwen2_5_VLForConditionalGeneration, AutoTokenizer, AutoProcessor
-from deepseek_vl.models import VLChatProcessor, MultiModalityCausalLM
+from deepseek_vl.models import VLChatProcessor, MultiModalityCausalLM,DeepseekVLV2Processor, DeepseekVLV2ForCausalLM
 from deepseek_vl.utils.io import load_pil_images
-
 from qwen_vl_utils import process_vision_info
-
+from janus.models import MultiModalityCausalLM, VLChatProcessor
+from janus.utils.io import load_pil_images
 
 os.environ["HF_HUB_OFFLINE"] = "1"  # 强制使用本地文件
 os.environ["TRANSFORMERS_OFFLINE"] = "1"  # 禁用在线检查
 
+class JanusAgent:
+    def __init__(self, model_path="../../Janus-Pro-7B"):
+        self.processor: VLChatProcessor = VLChatProcessor.from_pretrained(model_path)
+        self.tokenizer = self.processor.tokenizer
+
+        self.model: MultiModalityCausalLM = AutoModelForCausalLM.from_pretrained(
+            model_path, trust_remote_code=True
+        )
+        self.model = self.model.to(torch.bfloat16).cuda().eval()
+
+    def get_action(self, image: Image.Image, prompt: str) -> int:
+        conversation = [
+            {
+                "role": "<|User|>",
+                "content": f"<image_placeholder>\n{prompt}",
+                "images": [image],
+            },
+            {"role": "<|Assistant|>", "content": ""},
+        ]
+
+        pil_images = load_pil_images(conversation)
+
+        prepare_inputs = self.processor(
+            conversations=conversation,
+            images=pil_images,
+            force_batchify=True
+        ).to(self.model.device)
+
+        inputs_embeds = self.model.prepare_inputs_embeds(**prepare_inputs)
+
+        outputs = self.model.language_model.generate(
+            inputs_embeds=inputs_embeds,
+            attention_mask=prepare_inputs.attention_mask,
+            pad_token_id=self.tokenizer.eos_token_id,
+            bos_token_id=self.tokenizer.bos_token_id,
+            eos_token_id=self.tokenizer.eos_token_id,
+            max_new_tokens=10,
+            do_sample=False,
+            use_cache=True
+        )
+
+        answer = self.tokenizer.decode(outputs[0].cpu().tolist(), skip_special_tokens=True).strip()
+
+        try:
+            action = int(answer)
+            if action in [0, 1, 2]:
+                return action
+        except:
+            pass
+
+        return 0  # 默认返回前进
+
+
+class DeepseekVL2Agent:
+    def __init__(self, model_path="../../deepseek-vl2-tiny"):
+        self.processor: DeepseekVLV2Processor = DeepseekVLV2Processor.from_pretrained(model_path)
+        self.tokenizer = self.processor.tokenizer
+
+        self.model: DeepseekVLV2ForCausalLM = AutoModelForCausalLM.from_pretrained(
+            model_path, trust_remote_code=True
+        )
+        self.model = self.model.to(torch.bfloat16).cuda().eval()
+
+    def get_action(self, image: Image.Image, prompt: str) -> int:
+        conversation = [
+            {
+                "role": "<|User|>",
+                "content": "<image>\n" + prompt,
+                "images": [image]
+            },
+            {
+                "role": "<|Assistant|>",
+                "content": ""
+            }
+        ]
+
+        pil_images = load_pil_images(conversation)
+
+        prepare_inputs = self.processor(
+            conversations=conversation,
+            images=pil_images,
+            force_batchify=True,
+            system_prompt=""
+        ).to(self.model.device)
+
+        inputs_embeds = self.model.prepare_inputs_embeds(**prepare_inputs)
+
+        outputs = self.model.language_model.generate(
+            inputs_embeds=inputs_embeds,
+            attention_mask=prepare_inputs.attention_mask,
+            pad_token_id=self.tokenizer.eos_token_id,
+            bos_token_id=self.tokenizer.bos_token_id,
+            eos_token_id=self.tokenizer.eos_token_id,
+            max_new_tokens=10,
+            do_sample=False,
+            use_cache=True
+        )
+
+        answer = self.tokenizer.decode(outputs[0].cpu().tolist(), skip_special_tokens=True).strip()
+
+        try:
+            action = int(answer)
+            if action in [0, 1, 2]:
+                return action
+        except:
+            pass
+
+        return 0  # 默认返回前进
 
 class DSVL7BAgent:
     def __init__(self, model_path="../../deepseek-vl-7b-chat"):
@@ -95,12 +202,13 @@ class QwenVLAgent:
         )
 
         # 视觉信息预处理
-        image_inputs, _ = process_vision_info(messages)
+        image_inputs, video_inputs = process_vision_info(messages)
 
         # 构建最终输入
         inputs = self.processor(
             text=[text],
             images=image_inputs,
+            videos=video_inputs,
             padding=True,
             return_tensors="pt"
         ).to(self.model.device)
--- scripts/LLM_test.py
+++ scripts/LLM_test.py
@@ -2,7 +2,7 @@ from avp_env.metrics.metrics import get_parking_metrics
 from avp_env.metrics.utils import instru_len
 from avp_env.metrics.experiment import run_experiments, load_experiments
 from avp_env.envs.avp_env import MetricsVLLMEnv
-from avp_env.agents.LLM_agent import DSVL7BAgent, QwenVLAgent
+from avp_env.agents.LLM_agent import DSVL7BAgent, QwenVLAgent, DeepseekVL2Agent, JanusAgent
 import argparse
 import os
 
@@ -32,7 +32,10 @@ if __name__ == "__main__":
                     agent = DSVL7BAgent()
                 elif model == 'Qwen2.5-VL-7B-Instruct':
                     agent = QwenVLAgent()
-
+                elif model == 'deepseek-vl2-tiny':
+                    agent = DeepseekVL2Agent()
+                elif model == 'Janus-Pro-7B':
+                    agent = JanusAgent()
                 else:
                     raise ValueError(f"Invalid model name '{model}'. Please check your input.")
 
