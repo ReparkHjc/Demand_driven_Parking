ff91c58f4f990e881a5c8d0bd0669bf5a981775e
ilm75862 <2540395301qq.com>
2025-05-10 11:25:26 +0800

    debug

--- avp_env/agents/LLM_agent.py
+++ avp_env/agents/LLM_agent.py
@@ -7,7 +7,7 @@ from PIL import Image
 from transformers import AutoModelForCausalLM, Qwen2_5_VLForConditionalGeneration, AutoTokenizer, AutoProcessor
 from deepseek_vl.models import VLChatProcessor, MultiModalityCausalLM
 from deepseek_vl.utils.io import load_pil_images
-from deepseek_vl2.models import DeepseekVLV2Processor, DeepseekVLV2ForCausalLM
+# from deepseek_vl2.models import DeepseekVLV2Processor, DeepseekVLV2ForCausalLM
 
 from qwen_vl_utils import process_vision_info
 from janus.models import MultiModalityCausalLM, VLChatProcessor
