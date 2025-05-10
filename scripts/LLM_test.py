64bf5a2cc3ad3304869555efbdce666a64dc4aec
ilm75862 <2540395301qq.com>
2025-05-10 09:37:54 +0800

    debug

--- scripts/LLM_test.py
+++ scripts/LLM_test.py
@@ -40,5 +40,5 @@ if __name__ == "__main__":
                     experiments = run_experiments(env, agent, instru_num, output_file, view)
 
                 metrics = get_parking_metrics(experiments)
-                print("=" * 20, f"Metrics for instr_type '{instr_type}', model '{model}', view '{view}':\n", metrics,
-                      "=" * 20)
+                print("=" * 20, f"\nMetrics for instr_type '{instr_type}', model '{model}', view '{view}':\n", metrics,
+                      "\n", "=" * 20)
