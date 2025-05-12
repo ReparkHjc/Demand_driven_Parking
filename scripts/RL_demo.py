f16fc038640639e11833cec5610fd0a349e1df47
ilm75862 <2540395301qq.com>
2025-05-12 09:08:59 +0800

    debug

--- scripts/RL_demo.py
+++ scripts/RL_demo.py
@@ -41,7 +41,7 @@ def run_algorithm(algo_config, algo_name, total_timesteps):
     os.makedirs(checkpoint_dir, exist_ok=True)
     algo_config = algo_config.training(gamma=0.9, lr=1e-4)
     algo_config = algo_config.resources(num_gpus=1)
-    algo_config = algo_config.rollouts(num_rollout_workers=num_workers,episode_horizon=200)
+    algo_config = algo_config.rollouts(num_rollout_workers=num_workers)
 
     # algo_config = algo_config.environment(env=AutonomousParkingEnv)
     algo_config = algo_config.environment(
