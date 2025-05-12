1e86e7e7c60842e47462bd7cb97f988580c42ece
ilm75862 <2540395301qq.com>
2025-05-12 08:03:19 +0800

    debug

--- avp_env/envs/avp_env.py
+++ avp_env/envs/avp_env.py
@@ -59,7 +59,7 @@ class AutonomousParkingEnv(gym.Env):
         self.target_instruction = random.choice(self.metrics_instructions)
         instruction_tokens = self.tokenizer.encode(
             self.target_instruction.instruction, add_special_tokens=True,
-            max_length=self.max_string_length, pad_to_max_length=True,
+            max_length=self.max_string_length, padding='max_length',
             truncation=True
         )
 
